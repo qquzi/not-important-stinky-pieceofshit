63aa326638f2942110fc0bedbd270106e7612fb2
qquzi <anonymoususer29211@gmail.com>
2026-06-26 19:22:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const { LuauFactory } = require('luau-web');
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { LuauFactory } from 'luau-web';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
